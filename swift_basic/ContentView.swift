4863ae3d1fbd40137dcd9777d7f467c5cada8c0a
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-01-11 13:01:42 +0600

    basic counter with swift

--- swift_basic/ContentView.swift
+++ swift_basic/ContentView.swift
@@ -1,15 +1,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var count = 0
     var body: some View {
-        Image("niagarafalls")
-            .resizable()
-            .cornerRadius(20).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            .aspectRatio(contentMode: .fit)
-        Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()   // 👈 full screen background
 
+            VStack(spacing: 20) {
+                Image("niagarafalls")
+                    .resizable()
+                    .cornerRadius(20)
+                    .padding()
+                    .aspectRatio(contentMode: .fit)
+
+                Text("Placeholder")
+                    .font(.largeTitle)
+                
+                Text("Counter App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("\(count)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                HStack{
+                    Button(action:{
+                        count = count - 1
+                    }){
+                        Text("-")
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.white)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    }
+                    
+                    Button(action:{
+                        count = count + 1
+                    }){
+                        Text("+")
+                            .font(.largeTitle)
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.white)
+                            .background(Color.green)
+                            .cornerRadius(10)
+                        
+                    }
+                }
+            }
+        }
+    }
+
+}
+    
 #Preview {
     ContentView()
 }
